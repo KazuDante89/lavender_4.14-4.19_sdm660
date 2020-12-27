a823e9fd98cd1231310a19b1f5b943ebae611429
KazuDante89 <karlemmanueldorvil@gmail.com>
2020-12-26 20:34:36 -0500

    Test some changes out 	modified:   .circleci/build.sh 	modified:   arch/arm64/configs/lavender_defconfig 	modified:   drivers/cpufreq/Kconfig 	modified:   drivers/cpufreq/cpufreq.c 	modified:   kernel/Kconfig.hz 	modified:   kernel/sched/fair.c 	modified:   mm/Kconfig 	modified:   mm/vmstat.c

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -60,7 +60,7 @@ function compile() {
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 Test_v0.0.2.zip *
+    zip -r9 [EAS-4.14]-Crysis-Kernel_v0.0.zip *
     cd ..
 }
 sticker
